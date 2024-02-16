0e69848e5c9dc5ba7ea2f00966c43f637e125d6d
evanstolk <105766808+evanstolk@users.noreply.github.com>
2024-02-15 20:53:58 -0700

    Reverting changes

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see kart stats")
 
-link = '<a href="https://evanstolk.github.io/github_assignment" target="_blank">My Github Site</a>'
+link = '[Link to my Github Pages Site](https://evanstolk.github.io/github_assignment)'
 st.markdown(link, unsafe_allow_html=True)
